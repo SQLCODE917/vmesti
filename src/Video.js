de7aff71469a0174c167780f957d3ac848c87b3d
Dmitri S <ser.dmitri@gmail.com>
2021-04-10 02:23:37 -0400

    Style update, logs, auto-adjust levels

--- src/Video.js
+++ src/Video.js
@@ -5,16 +5,23 @@ import './Video.css';
 const VIDEO_SRC = "https://10380e91fda5e303.mediapackage.us-west-2.amazonaws.com/out/v1/3f767e030a234db8a9d7ef715d41a809/index.m3u8";
 
 function Video () {
-  const [qualityLevels, setQualityLevels] = useState([]);
   const [hlsInstance, setHls] = useState(null);
-  const onQualitySelect = useCallback((e) => {
-    console.log("ON SELECT", e.target.value);
-    const newLevel = parseInt(e.target.value, 10) || -1;
-    console.log("HLS INSTANCE", hlsInstance);
-    hlsInstance?.levelController?.loadLevel(newLevel);
-  }, [hlsInstance]);
+  const [logs, setLogs] = useState(['Loading...']);
+  const [loading, setLoading] = useState(false);
+
+  const onLevelEffective = useCallback((event, data) => {
+    const { level } = data;
+    setLogs([...logs,
+      `Effective Quality Level: ${level}`]);
+    setLoading(false);
+  }, [logs])
+
   const videoRef = useRef(null);
+
   useEffect(() => {
+    if(hlsInstance) {
+      return;
+    }
     const video = videoRef.current;
     if (video.canPlayType('application/vnd.apple.mpegurl')) {
       video.src = VIDEO_SRC;
@@ -27,36 +34,72 @@ function Video () {
       hls.loadSource(VIDEO_SRC);
       hls.attachMedia(video);
 			hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
-        console.log("Levels", data.levels);
-        setQualityLevels(data.levels);
+        console.log("Manifest Parsed", data);
       });
 
       hls.on(Hls.Events.LEVEL_SWITCHING, (event, data) => {
-        console.log("Level Switch Requested", data);
+        const { level, attrs: { RESOLUTION } } = data;
+        setLogs([...logs,
+          `Switching to Quality Level ${level} [${RESOLUTION}]...`
+        ]);
+        setLoading(true);
       });
-      hls.on(Hls.Events.LEVEL_SWITCHED, (event, data) => {
-        console.log("Level Switch is Effective", data);
-      });
+      hls.on(Hls.Events.LEVEL_SWITCHED, onLevelEffective);
+
+      hls.on(Hls.Events.ERROR, (event, data) => {
+        if (data.fatal) {
+          switch (data.type) {
+            case Hls.ErrorTypes.NETWORK_ERROR:
+              // try to recover network error
+              setLogs([...logs,
+                'Fatal network error encountered, trying to recover...'
+              ]);
+              console.log(data);
+              hls.startLoad();
+              break;
+            case Hls.ErrorTypes.MEDIA_ERROR:
+              setLogs([...logs,
+              'Fatal media error encountered, trying to recover...'
+              ]);
+              console.log(data);
+              hls.recoverMediaError();
+              break;
+            default:
+              // cannot recover
+              setLogs([...logs,
+                'Fatal error encountered, cannot recover, self-destructing.']);
+              console.log(data);
+              hls.destroy();
+              break;
+          }
+        } else {
+          setLogs([...logs,
+            `Non-fatal error encountered [${data.type}:${data.event}].`
+          ]);
+          console.log(data);
+        }
+      })
     } 
-  }, [videoRef])
-  const currentLevelIndex = hlsInstance?.levelController?.currentLevelIndex || -1;
-  console.log("CURRENT LEVEL INDEX", currentLevelIndex);
+  }, [videoRef, hlsInstance, onLevelEffective, logs])
+
+  const containerClass = loading ? "loading" : "";
   return(
-    <section id="videoContainer">
+    <section id="videoContainer"
+      className={containerClass}>
       <video controls id="video" ref={videoRef}></video>
-      <aside id="videoControls">
-        <div>
-          <label id="levelsLabel" htmlFor="levels">Quality</label>
-          <select label="levels"
-            onChange={onQualitySelect}
-            value={currentLevelIndex}>
-            <option value="-1">Auto</option>
-            {qualityLevels.map((level, index) => 
-              (<option key={index} value={index}>{level.attrs.RESOLUTION}</option>)
-            )}
-          </select>
-        </div>
-      </aside>
+      <footer>
+        <ul id="logs" className="lcd-text">
+          {logs.map((message, key) => (
+              <li key={key}>
+                {message}<span className="cursor">&nbsp;</span>
+              </li>
+            )
+          )}
+        </ul>
+      </footer>
+      <article id="grid-container">
+        <section id="grid"/>
+      </article>
     </section>
   );
 }
